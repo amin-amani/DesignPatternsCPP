1796008d5422642bdce97dab30cb23bd50ec22b6
amin-amani <didi1364@gmail.com>
2024-06-22 01:36:19 +0400

    docs: add discription for SFINAE.cpp

--- ModernCPP/SFINAE.cpp
+++ ModernCPP/SFINAE.cpp
@@ -1,3 +1,5 @@
 // Substitution Failure is Not an Error
 // https://www.youtube.com/watch?v=-Z7EOWVkb3M
+// The <type_traits> can be used to guide overload resolutions by using SFINAE. 
+//This allows the users to enable or disable function overloads based on the type properties
 #include <type_traits>
