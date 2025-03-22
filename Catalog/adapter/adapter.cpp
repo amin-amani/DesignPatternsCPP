213bb6e5dd1dbea7458542789de7cbd6bf0663c8
amin-amani <didi1364@gmail.com>
2025-03-22 20:54:43 +0400

    feat: write simple adapter pattern example

--- Catalog/adapter/adapter.cpp
+++ Catalog/adapter/adapter.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include <string>
 
+//==================================================
+class SimplePrinter {
+public:
+    void print(std::string text) {
+        std::cout << text << std::endl;
+    }
+};
+
+//==================================================
+class Printer {
+public:
+    virtual void print(std::string text) = 0; 
+    virtual ~Printer() {} 
+};
+
+//==================================================
+
+class MarkupPrinter : public Printer {
+    SimplePrinter *p; 
+public:
+    
+    MarkupPrinter(SimplePrinter *printer) {
+        p=printer;
+    }
+
+    
+    ~MarkupPrinter() override {
+      
+    }
+
+    void print(std::string text) override {
+        std::string new_str = "** " + text + " **";
+        p->print(new_str);
+    }
+};
+
+//==================================================
+int main() {
+    SimplePrinter sp;
+    Printer* p = new MarkupPrinter(&sp); 
+    p->print("Hello, World!"); 
+    
+    delete p; 
+    return 0;
+}
